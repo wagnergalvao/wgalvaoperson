48b65684839893902b24531dcabc5c9066f7096c
Wagner Galvão <galvao.wagner@outlook.com.br>
2022-10-10 21:44:28 -0300

    class 91 Custom JSON Serialization

--- src/main/java/br/com/wgalvao/wgalvaoperson/data/vo/v1/PersonVO.java
+++ src/main/java/br/com/wgalvao/wgalvaoperson/data/vo/v1/PersonVO.java
@@ -2,14 +2,25 @@ package br.com.wgalvao.wgalvaoperson.data.vo.v1;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({ "id", "first_name", "last_name", "gender", "address" })
 public class PersonVO implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     private Long id;
+
+    @JsonProperty("first_name")
     private String firstName;
+
+    @JsonProperty("last_name")
     private String lastName;
     private String address;
+
+    @JsonIgnore
     private String gender;
 
     public PersonVO() {
